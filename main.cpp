89719c3692428f27d3f20440625007f9cc4c72dd
ANA-KAREN-MARBAN <159966138+ANA-KAREN-MARBAN@users.noreply.github.com>
2024-02-20 00:18:49 +0000

    Version 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <Mascota.hpp>
+#include <Alimento.hpp>
+
 using namespace std;
+
 int main()
 {
-cout<<"Hola_"<<"23110149"<<endl;
+    Mascota m1;
+    Mascota m2;
+
+    Alimento a1(5);
+    Alimento a2(2);
+
+    m1.comer(a1);
+    m2.comer(a2);
+
+    cout << "Mascota 1 Energia:"<< m1.LeerEnergia() << endl
+    <<"Mascota 2 Energia:" << m2.LeerEnergia()<<endl;
+
     return 0;
-}
+};
 
