63482e6370965145c21060ecccf23a7635ce8fd2
ANA-KAREN-MARBAN <159966138+ANA-KAREN-MARBAN@users.noreply.github.com>
2024-03-05 00:19:07 +0000

    Version 5

--- include/Mascota.hpp
+++ include/Mascota.hpp
@@ -1,29 +1,44 @@
 #pragma once
 #include <Alimento.hpp>
+
+std::string nombre;
+
 class Mascota
 {
-    private:
+private:
     int Energia;
     int Felicidad;
 
-    public:
-    Mascota(){
+    std::string nombre;
+
+public:
+    Mascota(std::string nombre)
+    {
+        this->nombre = nombre;
         this->Energia = 0;
         this->Felicidad = 0;
     }
-    ~Mascota(){}
-    void comer(Alimento alimento){
-    Energia += alimento.ExtraerEnergia();
-}
-void Jugar(){
-    Energia -= 1;
-    Felicidad += 1;
+    ~Mascota() {}
 
-}
-int LeerFelicidad(){
-    return this ->Felicidad;
-}
-int LeerEnergia(){
-    return this ->Energia;
-}
+    void comer(Alimento alimento)
+    {
+        Energia += alimento.ExtraerEnergia();
+    }
+    void Jugar()
+    {
+        Energia -= 1;
+        Felicidad += 1;
+    }
+    int LeerFelicidad()
+    {
+        return this->Felicidad;
+    }
+    int LeerEnergia()
+    {
+        return this->Energia;
+    }
+    std::string Leernombre()
+    {
+        return this->nombre;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
+#include <list>
 #include <iostream>
 #include <Mascota.hpp>
 #include <Alimento.hpp>
 
-using namespace std;
+// using namespace std;
 
 int main()
 {
-    Mascota m1;
-    Mascota m2;
+    std::list<Mascota> mascotas;
 
     Alimento a1(5);
     Alimento a2(2);
 
+    Mascota m1("Firulais");
+    Mascota m2("Roko");
+
+    mascotas.push_back(m1);
+    mascotas.push_back(m2);
+
     m1.comer(a1);
     m2.comer(a2);
 
-    cout << "Mascota 1 Energia:"<< m1.LeerEnergia() << endl
-    <<"Mascota 2 Energia:" << m2.LeerEnergia()<<endl;
+    for (auto &&Mascota : mascotas)
+    {
+        std ::cout
+            << Mascota.Leernombre()
+            << ":"
+            << Mascota.LeerEnergia()
+            << std::endl;
+    }
 
     return 0;
 };
-
